e57354f2dfe1c2b903ef2a7d9af89b1d3daf4679
Matt Frost <mfrost@Matts-MacBook-Air.local>
2014-03-05 16:10:59 -0500

    Added some shiny menus

--- ui.R
+++ ui.R
@@ -10,13 +10,21 @@ require(scales)
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Insurance Estimator"),
+  headerPanel(
+    "Insurance Estimator"
+    ),
+  
   
   sidebarPanel(
-    p('These will eventually become dropdown menus.'),
+    p('How much you pay for health insurance and medical care depends on forces beyond your control
+      as well as the choices you make when you buy insurance. This gadget is a tool for investigating
+      your health care costs across hundreds of simulated years, based on the cost profiles of different
+      plans available via the Obamacare exchanges.'),
+    p('This is a work in progress. Both the interface and the cost estimation process have a long way to go.'),
+    p('Find me on twitter ( @mattfrost ) if you have any questions or comments.'),
     textInput("iST", "ST:", "VA"),
     textInput("iCounty", "COUNTY:", "ALBEMARLE"),
-    
+    p('The state and county input fields will eventually become dropdown menus.'),
     sliderInput('iSims', 'Number of simulations to run', min=100, max=1000,
                 value=200, step=100, round=0)
     
@@ -34,6 +42,8 @@ shinyUI(pageWithSidebar(
       (orange box), deductible costs paid at 100% (yellow box), and post-deductible copays 
       (the amount between the yellow box and the total cost). The red line is the functional
       ceiling, or the sum of the premiums and the out-of-pocket maximum.'),
+    p('The gray boxes show the likelihood of a certain net cost range. If the box with a height of 40% is 
+      around $20,000, it means that in 40% of the simulated years, the net cost was $20,000.'),
     plotOutput("dist.plot", height=2600),
     h3(textOutput("plan.caption")),
     tableOutput("tab.plans"),
