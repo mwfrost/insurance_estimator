f4737d84e7b0340db95ac981abfd1867efc2d9d9
Matt Frost <mwfrost@fastmail.fm>
2016-04-09 23:35:57 -0700

    Added random library chance.js; built pieces of simulation steps

--- app.js
+++ app.js
@@ -1,15 +1,176 @@
 
+
+
 angular.module("app", ["chart.js","ui.grid", "ui.grid.edit"]).controller("InsCtrl", function ($scope) {
 
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Option A', 'Option B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+
+  //
+  // Data Definition
+  //
+    $scope.Plans = [
+      {
+        "planName": "Bronze Plan" ,
+        "premiumFamily": 2279,
+        "deductibleFamily": 6000,
+        "maxOOPFamily": 11900
+      },
+      {
+        "planName": "Silver Plan" ,
+        "premiumFamily": 3526,
+        "deductibleFamily": 3000,
+        "maxOOPFamily": 6850
+      }
+    ];
+
+    $scope.getPlanNames = function () {
+      var planNames = [];
+      angular.forEach($scope.Plans, function(plan){
+        this.push(plan.planName);
+      }, planNames);
+        return planNames;
+    };
+
+    $scope.simulateYear = function () {
+
+    // -------------------
+    // iterate through each plan option, family member, and scenario year
+    // accumulate all the costs within each plan option and return a histogram
+    // -------------------
+      // for each plan option
+      var SimulatedYear = [];
+      angular.forEach($scope.Plans, function(plan){
+
+        console.log('Add premium to random costs for plan '.concat(plan.planName));
+        var VarCosts = [];
+        VarCosts = chance.n(chance.normal, 7, {mean: 2000, dev: 1000, fixed: 2}) ;
+        console.log('Before adding premium of '.concat(plan.premiumFamily));
+        console.log(VarCosts);
+        var PlanCosts = [];
+        PlanCosts = VarCosts.map(function(cost) {
+            return cost + plan.premiumFamily;
+          });
+        console.log('After adding premium');
+        console.log(PlanCosts);
+        this.push({planname: plan.planName, costs: PlanCosts});
+      }, SimulatedYear);
+
+        // for each scenario run
+
+
+          // for each family member
+
+    // within each plan option, count the results by cost bin
+
+          // var CostCurves = [
+          //   chance.n(chance.normal, 7, {mean: 100, fixed: 7}),
+          //   chance.n(chance.normal, 7, {mean: 100, fixed: 7})
+          // ];
+        return SimulatedYear ;
+    };
+
+    $scope.SimulatedYear = $scope.simulateYear();
+
+
+    $scope.getPlanCosts = function () {
+      console.log('executing getPlanCosts()');
+      console.log('SimulatedYear object:'.concat($scope.SimulatedYear));
+      var PlanCosts = [];
+      angular.forEach($scope.SimulatedYear, function(simyear){
+        console.log('simyear:');
+        console.log(simyear);
+        console.log('simresult.costs:');
+        console.log(simyear.costs);
+        this.push(simyear.costs);
+      }, PlanCosts);
+      console.log('PlanCosts returned from getPlanCosts():');
+      console.log(PlanCosts);
+        return PlanCosts;
+    };
+
+  $scope.freqlabels = [1,2,3,4,5,6,7];
+//  $scope.series = ['Option A', 'Option B'];
+  $scope.series = $scope.getPlanNames();
+
+  $scope.costcurves =
+    $scope.getPlanCosts();
+  ;
+
   $scope.onClick = function (points, evt) {
-    console.log(points, evt);
+    //console.log(points, evt);
   };
+
+
+
+  $scope.FamilyProfile = [          {
+                    "name": "Insured" ,
+                    "include": true,
+                    "age": 40,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Spouse of Insured" ,
+                    "include": true,
+                    "age": 40,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 1" ,
+                    "include": true,
+                    "age": 17,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 2" ,
+                    "include": true,
+                    "age": 15,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 3" ,
+                    "include": true,
+                    "age": 13,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 4" ,
+                    "include": true,
+                    "age": 11,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 5" ,
+                    "include": true,
+                    "age": 9,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  },
+                  {
+                    "name": "Child 6" ,
+                    "include": true,
+                    "age": 7,
+                    "visitBase": 150,
+                    "sickRisk": 0.1,
+                    "catRisk": 0.01
+                  }
+
+             ]
+
+
+
+
   $scope.gridFamilyOptions = {
           enableSorting: true,
           columnDefs: [
@@ -20,72 +181,7 @@ angular.module("app", ["chart.js","ui.grid", "ui.grid.edit"]).controller("InsCtr
             { name:'Sick Risk' , field: 'sickRisk'},
             { name:'Catastrophic Risk' , field:'catRisk'}
           ],
-          data : [          {
-                            "name": "Insured" ,
-                            "include": true,
-                            "age": 40,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Spouse of Insured" ,
-                            "include": true,
-                            "age": 40,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 1" ,
-                            "include": true,
-                            "age": 17,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 2" ,
-                            "include": true,
-                            "age": 15,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 3" ,
-                            "include": true,
-                            "age": 13,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 4" ,
-                            "include": true,
-                            "age": 11,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 5" ,
-                            "include": true,
-                            "age": 9,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          },
-                          {
-                            "name": "Child 6" ,
-                            "include": true,
-                            "age": 7,
-                            "visitBase": 150,
-                            "sickRisk": 0.1,
-                            "catRisk": 0.01
-                          }
-
-                     ]
+          data : $scope.FamilyProfile
         };
 
 $scope.gridPlanOptions = {
@@ -96,24 +192,10 @@ $scope.gridPlanOptions = {
           { name:'Deductible', field: 'deductibleFamily'},
           { name:'Max OOP', field: 'maxOOPFamily'}
         ],
-        data : [
-          {
-            "planName": "Bronze Plan" ,
-            "premiumFamily": 2279,
-            "deductibleFamily": 6000,
-            "maxOOPFamily": 11900
-          },
-          {
-            "planName": "Silver Plan" ,
-            "premiumFamily": 3526,
-            "deductibleFamily": 3000,
-            "maxOOPFamily": 6850
-          }
-                   ]
+        data : $scope.Plans
       };
 
 
-$scope.planList = $scope.gridPlanOptions.data;
 
 
 });
